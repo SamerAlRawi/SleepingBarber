2357b3d10d5790d264d4ecbb75e0155a70d13f47
Samer <samer_control@yahoo.com>
2016-11-12 12:39:31 -0500

    updated release.bat

--- release.bat
+++ release.bat
@@ -2,6 +2,8 @@
 :Loop
 IF "%1"=="" GOTO Continue
    nuget pack SleepingBarber.Persistence.RavenDB.nuspec -version %1
+   nuget pack SleepingBarber.Logging.nuspec -version %1
+   nuget pack SleepingBarber.Logging.RavenDB.nuspec -version %1
    nuget pack SleepingBarber.nuspec -version %1
 SHIFT
 GOTO Loop
