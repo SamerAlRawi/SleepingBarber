9392113aeb88010ba60bcc1337aeafe139ed0fba
Samer <samer_control@yahoo.com>
2016-11-13 18:48:03 -0500

    created publish.bat and release.bat is running msbuild and nunit tests prior to packaging

--- release.bat
+++ release.bat
@@ -1,4 +1,17 @@
 @ECHO OFF
+
+MSBuild.exe SleepingBarber.sln /t:Build /p:Configuration=Release
+if /I "%ERRORLEVEL%" NEQ "0" (
+   echo MSBUILD Task failed %ERRORLEVEL%
+   exit /b %ERRORLEVEL%
+)
+
+nunit3-console.exe ".\SleepingBarber.Tests\bin\Release\SleepingBarber.Tests.dll" --output=result.xml
+if /I "%ERRORLEVEL%" NEQ "0" (
+   echo NUNIT Task failed %ERRORLEVEL%
+   exit /b %ERRORLEVEL%
+)
+
 :Loop
 IF "%1"=="" GOTO Continue
    nuget pack SleepingBarber.Persistence.RavenDB.nuspec -version %1
@@ -8,3 +21,4 @@ IF "%1"=="" GOTO Continue
 SHIFT
 GOTO Loop
 :Continue
+
